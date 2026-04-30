0809cbc6e35ecaefc8fe6bda777c817bb864df08
Ernes <ernescanoski15@gmail.com>
2026-04-30 17:11:12 +0200

    updated panel names

--- src/data/panels.js
+++ src/data/panels.js
@@ -1,61 +1,60 @@
 const panels = [
   {
-    id: "1 - 10AM",
+    id: "1 — 10AM",
     title: "Physical AI & Future Industry",
     moderator: "PhD Sefidanis Anis, CEO N99 Tech Park",
     speakers: [
-      "Stefan Andonovski - Minister of Digital Transformation, N.Macedonia",
-      "Kate Upevce Potter, Google AI Marketing & Sales",
+      "Stefan Andonovski — Minister of Digital Transformation, N.Macedonia",
+      "Kate Upevce Potter — Google AI Marketing & Sales",
       "Ljupco Georgiev, Regional Manager — Microsoft",
-      "Aldo Cingolani, Bertone Design Group - Italy", // maybe he will not come
+      "Anackova Maja — Assistant at the Institute of Mechanics, Faculty of Mechanical Engineering—Skopje", 
     ],
   },
   {
-    id: "2 - 11AM",
+    id: "2 — 11AM",
     title: "Digital Transformation, Education & Future Economy",
     moderator:
       "Dime Galapchev — FinqUp",
     speakers: [
-      "Vesna Janevska - Minister of Education, N.Macedonia",
-      "IT Chamber of Commerce", // ??????
+      "Vesna Janevska — Minister of Education, N.Macedonia",
+      "MASIT — IT Chamber of Commerce", // ??????
       "KOTRA — South Korea",
-      "ARIB - Saudi Arabia"
+      "Maximilian Foedinger — VIAAS, Vice President, Austria"
     ],
   },
   {
-    id: "3 - 12PM",
+    id: "3 — 12PM",
     title: "National Resilience & Cyber Defense",
     moderator: "PhD Kire Jakimoski",
     speakers: [
       // "Vlado Misajlovski — Minister of Defense, N. Macedonia",
       "Adam Benet, CEO — Red Piranha, Australia",
       "Jens Pauli Rasmunsen — Norliga, Danmark",
-      "Dr. Metodi Hadji Janev - Principal Advisor for Digital Resilience, CRDF Global",
+      "Dr. Metodi Hadji Janev — Principal Advisor for Digital Resilience, CRDF Global",
       "Fawaz — ARIB, Saudi Arabia",
-      "Filip Simeonov -CEO & Cyber Security expert vo Tessa Sec "
+      "Filip Simeonov — CEO & Cyber Security expert vo Tessa Sec "
     ],
   },
   {
-    id: "4 - 1PM",
+    id: "4 — 1PM",
     title: "Agrotech 5.0: Bridging Southeast Europe and Global Trends",
     moderator: "Prof. Oliver Iliev",
     speakers: [
-      "Cvetan Tripunovski - Minister of Agriculture N.Macedonia",
+      "Cvetan Tripunovski — Minister of Agriculture N.Macedonia",
       "J.S. Park, CEO — SUNGHO, South Korea",
       "Can Yurdakul, CEO — Ant Systems, Türkiye",
       "Wageningen University, Holland",
     ],
   },
   {
-    id: "5 - 2PM",
+    id: "5 — 2PM",
     title: "Startups & Investment",
-    moderator: "Igor Madzov · Focus: Innovation ecosystem & scaling",
+    moderator: "Igor Madzov — Innovation ecosystem & scaling",
     speakers: [
-      "Maximilian Foedinger - VIAAS, Vice President, Austria",
-      "Daniela Dimovska - Managing Director, Agency for Innovation, Scientific and Technological Development and Entrepreneurship – INOVA",
-      "Gjorgi Rafajlovski - Managing Partner Zephyr Angels, Intellectual Property expert",
-      "Gideon Marks - Serial Entrepreneur, Investor, and Google for Startups Anchor Mentor USA",
-      "Elena Hadzi Pecova - Manager Ceed Business Angels/ Adria Angels, Little Big Founders",
+      "Daniela Dimovska — Managing Director, Agency for Innovation, Scientific and Technological Development and Entrepreneurship – INOVA",
+      "Gjorgi Rafajlovski — Managing Partner Zephyr Angels, Intellectual Property expert",
+      "Gideon Marks — Serial Entrepreneur, Investor, and Google for Startups Anchor Mentor USA",
+      "Elena Hadzi Pecova — Manager Ceed Business Angels/ Adria Angels, Little Big Founders",
     ],
   },
 ];
